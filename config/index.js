19dd46ea6996955a1df22d07a37b695ef698f88d
chaibiying <17801069027@163.com>
2018-12-08 16:25:16 +0800

    ..忘记push

--- config/index.js
+++ config/index.js
@@ -20,6 +20,7 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {},
 
+    
     // Various Dev Server settings
     host: needHost, // can be overwritten by process.env.HOST
     port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
