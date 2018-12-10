f0e8a2d7443f217309f84ee801431af858ac33ee
chaibiying <17801069027@163.com>
2018-12-10 15:00:05 +0800

    兑换码接口

---
+++ src/api/bindInviteCode.js
@@ -0,0 +1,8 @@
+import request from "@common/request/axios";
+export function bindInviteCode(params) {
+  return request({
+    url: "/Users/bind_invite_code",
+    method: "post",
+    data: params
+  });
+}
